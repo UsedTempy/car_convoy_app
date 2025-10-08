8edd45461c5725677139df08f7a7930749441c36
Tempy <usedtemplate@gmail.com>
2025-10-08 09:56:09 +0200

    Removed unneeded devices

--- car_convoy_app/lib/main.dart
+++ car_convoy_app/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(const MyApp());
+  runApp(const Dashboard());
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
