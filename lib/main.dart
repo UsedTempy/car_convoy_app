bccaff4f13cc363b610abd98a934b3620df1d16a
Joost Kraan <2006joost@gmail.com>
2025-10-08 14:35:46 +0200

    firebase added to project

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,12 @@ import 'package:car_convoy_app/themes/app_colors.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   try {
     await dotenv.load(fileName: ".env");
   } catch (e) {
